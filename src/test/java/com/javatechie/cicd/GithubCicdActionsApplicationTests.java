3252ef19afe2fe2c1cc73712e53196d547f5ba5e
Bholanath <bholanath.das21@gmail.com>
2025-12-30 19:01:19 +0530

    Added files

---
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.javatechie.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
